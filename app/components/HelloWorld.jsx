23b6b51da7064433b8d7759046fea1f92700c244
Glitch (ambiguous-thinkable-maize) <none>
2020-04-21 18:31:54 +0000

    📹🏂 Checkpoint ./app/components/HelloWorld.jsx:6391310/2188

--- app/components/HelloWorld.jsx
+++ app/components/HelloWorld.jsx
@@ -5,23 +5,26 @@ import React, {useEffect, useState} from 'react';
 /* the main page for the index route of this app */
 const HelloWorld = function() {
   const [id, setId] = useState('<nimic>');
-  
+  const [msgs, setMsgs] = useState([]);
+
   useEffect(() => {
     console.log('useeffect');;;
     let socket = window.io();
     socket.on('onconnected', data => {
       console.log('ononnected', data);;;
       setId(data.id);
-    })
+    });
+    socket.on('chat message',  msg => {
+      
+    });
   }, []);
   
   return (
     <>
-      <ul>
-      <li>lorem</li>
+      <ul id="messages">
       </ul>
       <form action="">
-        <input id="m'" autoComplete="off" />
+        <input id="m" autoComplete="off" />
         <button>Sendxt</button>
       </form>
     </>
