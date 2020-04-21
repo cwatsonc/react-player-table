0a42221fcdf8d663b54191c6ee4217666587e6bb
Glitch (ambiguous-thinkable-maize) <none>
2020-04-21 18:43:53 +0000

    👡👯 Checkpoint ./app/components/HelloWorld.jsx:6391310/2247

--- app/components/HelloWorld.jsx
+++ app/components/HelloWorld.jsx
@@ -1,35 +1,39 @@
 // const React = require('react');
 // const {useEffect, useState} = React;
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 
 /* the main page for the index route of this app */
 const HelloWorld = function() {
-  const [id, setId] = useState('<nimic>');
-  const [msgs, setMsgs] = useState([]);
-
+  const [id, setId] = useState("<nimic>");
+  const [msgs, setMsgs] = useState(["lorem", "ipsum", "dolor"]);
+  const [input, setInput] = useState('');
+  
   useEffect(() => {
-    console.log('useeffect');;;
-    let socket = window.io();
-    socket.on('onconnected', data => {
-      console.log('ononnected', data);;;
+    console.log("useeffect");
+    const socket = window.io();
+    socket.on("onconnected", data => {
+      console.log("ononnected", data);
       setId(data.id);
     });
-    socket.on('chat message',  msg => {
-      
+    socket.on("chat message", msg => {
+      setMsgs(msgs => [...msgs, msg]);
     });
   }, []);
-  
+
   return (
     <>
       <ul id="messages">
+        {msgs.map((msg, i) => (
+          <li key={i}>{msg}</li>
+        ))}
       </ul>
-      <form action="">
+      <form onSubmit={onSubmit}>
         <input id="m" autoComplete="off" />
-        <button>Sendxt</button>
+        <button>Send</button>
       </form>
     </>
   );
-}
+};
 
 // module.exports = HelloWorld;
 export default HelloWorld;
