ac5a2d327e49866e8ec524e501aa125155bafd4d
Glitch (ambiguous-thinkable-maize) <none>
2020-04-21 20:55:27 +0000

    🌊🌎 Checkpoint ./server.js:6391310/6233 ./README.md:6391310/684

--- README.md
+++ README.md
@@ -16,22 +16,3 @@ Click `Show` in the header to see your app live. Updates to your code will insta
 
 Your Project
 ------------
-
-On the front-end,
-- edit files in `app` folder, it contains all the React stuff
-- drag in `assets`, like images or music, to add them to your project
-
-On the back-end,
-- your app starts at `server.js`
-- add frameworks and packages in `package.json`
-- safely store app secrets in `.env` (nobody can see this but you and people you invite)
-
-
-By [Glitch](https://glitch.com/)
--------------------
-
-Glitch is the friendly commmunity where you'll build the app of your dreams. Glitch lets you instantly create, remix, edit, and host an app, bot or site, and you can invite collaborators or helpers to simultaneously edit code with you.
-
-[Find out more](https://glitch.com/about).
-
-\ ゜o゜)ノ
--- server.js
+++ server.js
@@ -1,51 +1,26 @@
-const io              = require('socket.io');
-const express         = require('express');
-const http            = require('http');
-const app             = express();
-const server          = http.createServer(app);
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.get("/", function(request, response) {
-  response.sendFile(__dirname + '/app/index.html');
+  response.sendFile(__dirname + "/app/index.html");
 });
 
-server.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + server.address().port);
+http.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + http.address().port);
 });
 
+io.on("connection", function(socket) {
+  console.log("a user connected");
 
-let nextId = 0;
-let nConnections = 0;
+  socket.on("chat message", function(msg) {
+    io.emit("chat message", msg);
+  });
 
-    var sio = io(server);
-
-
-        //Socket.io will call this function when a client connects,
-        //So we can send that client looking for a game to play,
-        //as well as give that client a unique ID to use so we can
-        //maintain the list if players.
-    sio.on('connection', function (client) {
-            //tell the player they connected, giving them their id
-      client.userid = nextId++;
-      ++nConnections;
-        client.emit('onconnected', { id: client.userid} );
-      console.log('new connection', client.userid, nConnections, 'total');
-      
-        client.on('chat message', function(msg) {
-            sio.emit('chat message', msg);
-        }); //client.on message
-
-            //When this client disconnects, we want to tell the game server
-            //about that as well, so it can remove them from the game they are
-            //in, and make sure the other player knows that they left and so on.
-        client.on('disconnect', function () {
-
-                //Useful to know when soomeone disconnects
-            console.log('\t socket.io:: client disconnected', client.userid);
-          --nConnections;
-          console.log(nConnections, 'total');
-            
-        }); //client.on disconnect
-     
-    }); //sio.sockets.on connection
+  socket.on("disconnect", function() {
+    console.log("a user disconnected");
+  });
+});
